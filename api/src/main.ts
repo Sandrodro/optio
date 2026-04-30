c6a2d797a5dc116d03284658238a438fab811ce3
Sandrodro <sandrogach@gmail.com>
2026-04-30 14:02:43 +0400

    fix: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3000);
 }
